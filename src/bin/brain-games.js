17e58c34b6c1da5954436a215c6e422271fdd7fd
rikkiRikkardo <rikki.rikkardo44@gmail.com>
2019-12-01 15:03:52 +0500

    step 4

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 
-import {greet} from '..';
+import greet from '..';
+
 greet();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greet = () => {
-   console.log('Welcome to the Brain games!');
-   const userName = readlineSync.question('May I have your name? ');
-   console.log('Hello, ' + userName + '!');
-   };
+const greet = () => {
+  console.log('Welcome to the Brain games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+export default greet;
